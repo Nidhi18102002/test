e2d31324545d40be47b28cab8da5bd918857cced
Nidhi18102002 <nidhinagathan1810@gmail.com>
2025-07-31 12:47:00 +0530

    fixing corrupted files

--- load_data.py
+++ load_data.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import mysql.connector
+import csv
 
-df=pd.read_csv(r'C:\Users\siddh\OneDrive\Desktop\test\__MACOSX\archive\._products.csv',encoding='utf-8',encoding_errors='ignore')
+# df=pd.read_csv(r'C:\Users\siddh\OneDrive\Desktop\test\__MACOSX\archive\._products.csv',encoding='utf-8',encoding_errors='ignore')
 
 conn = mysql.connector.connect(
     host='localhost',
@@ -11,9 +12,16 @@ conn = mysql.connector.connect(
 )
 
 cursor = conn.cursor()
-for _, row in df.iterrows():
-    cursor.execute("INSERT INTO products (product_id, product_name, price, category)"  "VALUES (%s, %s, %s, %s)",
-                   (row['product_id'], row['product_name'], row['price'], row['category']))
+with open(r'C:\Users\siddh\OneDrive\Desktop\test\__MACOSX\archive\._products.csv', 'r', newline ='', encoding='utf-8', errors='ignore') as csvfile:
+    cleaned_file = (line.replace('\00', '') for line in csvfile)
+    reader = csv.reader(cleaned_file)
+    header = next(reader)
+    insert_query = f"INSERT INTO products({', '.join(header)}) VALUES ({', '.join(['%s'] * len(header))})"
+    row
+    for row in reader:
+        if len(row) == len(header):
+            cursor.execute(insert_query, row)
+            row_count += 1
 conn.commit()
 cursor.close()
 conn.close()
